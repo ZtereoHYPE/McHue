8c9d441439c328b8a21cdcad8c201c9d5938de8c
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2022-07-03 02:28:54 +0200

    change: cleanup McHue.java

--- src/main/java/codes/ztereohype/mchue/McHue.java
+++ src/main/java/codes/ztereohype/mchue/McHue.java
@@ -5,13 +5,13 @@ import codes.ztereohype.mchue.config.Config;
 import codes.ztereohype.mchue.config.ModProperties;
 import codes.ztereohype.mchue.devices.BridgeManager;
 import codes.ztereohype.mchue.devices.HueBridge;
+import codes.ztereohype.mchue.gui.screens.LightDebugScreen;
 import codes.ztereohype.mchue.gui.screens.LightSelectionScreen;
 import lombok.Getter;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.network.chat.Component;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,41 +23,42 @@ public class McHue implements ClientModInitializer {
     public static final String MOD_ID = "mchue";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static final BridgeManager BRIDGE_MANAGER = new BridgeManager();
-
+    public static final Config BRIDGE_DATA = new Config(Paths.get("./.mchue/bridge_data.config"),
+                                                        "McHue config containing sensitive data about the bridge. WARNING: DO NOT SHARE THIS UNDER ANY CIRCUMSTANCES",
+                                                        Map.of(BridgeProperties.BRIDGE_ID.getSettingName(), BridgeProperties.BRIDGE_ID.getDefaultValue(),
+                                                               BridgeProperties.BRIDGE_IP.getSettingName(), BridgeProperties.BRIDGE_IP.getDefaultValue(),
+                                                               BridgeProperties.DEVICE_INDENTIFIER.getSettingName(), BridgeProperties.DEVICE_INDENTIFIER.getDefaultValue(),
+                                                               BridgeProperties.USERNAME.getSettingName(), BridgeProperties.USERNAME.getDefaultValue(),
+                                                               BridgeProperties.CLIENT_KEY.getSettingName(), BridgeProperties.CLIENT_KEY.getDefaultValue(),
+                                                               BridgeProperties.CONNECTED_LIGHTS.getSettingName(), BridgeProperties.CONNECTED_LIGHTS.getDefaultValue()));
+    public static final Config SETTINGS_CONFIG = new Config(Paths.get("./config/mchue.config"),
+                                                            "McHue config containing general mod settings",
+                                                            Map.of(ModProperties.IS_ACTIVE.getSettingName(), ModProperties.IS_ACTIVE.getDefaultValue()));
     public static HueBridge activeBridge;
     public static LightSelectionScreen settingsScreen;
-
-    private String toastTitle;
+    //todo: move in correct mixin place (gui)
+    public static LightDebugScreen ld = new LightDebugScreen(Minecraft.getInstance());
     private @Getter static McHue initialisedInstance;
-
-    public static final Config BRIDGE_DATA = new Config(Paths.get("./.mchue/bridge_data.config"),
-            "McHue config containing sensitive data about the bridge. WARNING: DO NOT SHARE THIS UNDER ANY CIRCUMSTANCES",
-            Map.of(BridgeProperties.BRIDGE_ID.getSettingName(), BridgeProperties.BRIDGE_ID.getDefaultValue(),
-                    BridgeProperties.BRIDGE_IP.getSettingName(), BridgeProperties.BRIDGE_IP.getDefaultValue(),
-                    BridgeProperties.DEVICE_INDENTIFIER.getSettingName(), BridgeProperties.DEVICE_INDENTIFIER.getDefaultValue(),
-                    BridgeProperties.USERNAME.getSettingName(), BridgeProperties.USERNAME.getDefaultValue(),
-                    BridgeProperties.CLIENT_KEY.getSettingName(), BridgeProperties.CLIENT_KEY.getDefaultValue(),
-                    BridgeProperties.CONNECTED_LIGHTS.getSettingName(), BridgeProperties.CONNECTED_LIGHTS.getDefaultValue()));
-
-    public static final Config SETTINGS_CONFIG = new Config(Paths.get("./config/mchue.config"),
-            "McHue config containing general mod settings",
-            Map.of(ModProperties.IS_ACTIVE.getSettingName(), ModProperties.IS_ACTIVE.getDefaultValue()));
+    private String toastTitle;
 
     @Override
     public void onInitializeClient() {
         initialisedInstance = this;
+
+        setupConfig();
+        setupBridgeConnection();
+    }
+
+    private void setupConfig() {
         try {
             BRIDGE_DATA.initialise();
             SETTINGS_CONFIG.initialise();
         } catch (IOException e) {
-            LOGGER.log(Level.FATAL, "Could not read or write McHue config file.");
-            // TODO: crash the game? idk how to handle exceptions yet
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-
-        setupBridgeConnection();
     }
 
+    //todo: rethink this
     private void setupBridgeConnection() {
         boolean savedBridgeFoundLocally = BridgeManager.getLocalBridges(false).stream()
                                                        .anyMatch(b -> b.getBridgeId()
@@ -66,12 +67,22 @@ public class McHue implements ClientModInitializer {
         if (savedBridgeFoundLocally) {
             String id = BRIDGE_DATA.getProperty(BridgeProperties.BRIDGE_ID);
             String ip = BRIDGE_DATA.getProperty(BridgeProperties.BRIDGE_IP);
-            activeBridge = new HueBridge(id, ip);
+            String clientKey = BRIDGE_DATA.getProperty(BridgeProperties.CLIENT_KEY);
+            String username = BRIDGE_DATA.getProperty(BridgeProperties.USERNAME);
+            String deviceIdentifier = BRIDGE_DATA.getProperty(BridgeProperties.DEVICE_INDENTIFIER);
 
-            BRIDGE_MANAGER.completeBridge(activeBridge);
+            activeBridge = new HueBridge(id, ip, clientKey, username, deviceIdentifier);
+
+            String[] lights = BRIDGE_DATA.getPropertyArray(BridgeProperties.CONNECTED_LIGHTS);
+            if (lights != null) {
+                for (String lightId : lights) {
+                    activeBridge.setActiveLight(lightId, true);
+                }
+                return;
+            }
         }
 
-        toastTitle = !savedBridgeFoundLocally ? "Could not find Bridge." : "Set up the Hue Bridge.";
+        toastTitle = BRIDGE_DATA.getProperty(BridgeProperties.BRIDGE_ID) != null ? "Could not find Bridge." : "Set up the Hue Bridge.";
     }
 
     //todo: fix this bs -_-
