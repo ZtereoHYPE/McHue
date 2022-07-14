b1710f485ae870e5fcafcbac613a5e026857e6d4
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2022-07-15 00:07:12 +0200

    new: hsv test

--- src/test/java/codes/ztereohype/mchue/LightStateTest.java
+++ src/test/java/codes/ztereohype/mchue/LightStateTest.java
@@ -18,4 +18,18 @@ public class LightStateTest {
         assert(colour2.getGreenI() == 186);
         assert(colour2.getBlueI() == 96);
     }
+
+    @Test
+    public void testHsvToHsv() {
+        LightState colour1 = new LightState(359F,1F, 1F, true);
+        LightState colour2 = new LightState(134F, 0.63F, 0.73F, false);
+
+        assert(Math.round(colour1.getHue()*100)/100f == 359F);
+        assert(Math.round(colour1.getSaturation()*100)/100f == 1.0F);
+        assert(Math.round(colour1.getBrightness()*100)/100f == 1.0F);
+
+        assert(Math.round(colour2.getHue()*100)/100f == 134F);
+        assert(Math.round(colour2.getSaturation()*100)/100f == 0.63F);
+        assert(Math.round(colour2.getBrightness()*100)/100f == 0.73F);
+    }
 }
