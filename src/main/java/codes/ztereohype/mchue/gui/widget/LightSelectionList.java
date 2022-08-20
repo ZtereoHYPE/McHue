2a9f27152936fd75af1a1305df501d8a37b8a881
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2022-08-20 21:36:28 +0200

    change: nest enties into parent class

--- src/main/java/codes/ztereohype/mchue/gui/widget/LightSelectionList.java
+++ src/main/java/codes/ztereohype/mchue/gui/widget/LightSelectionList.java
@@ -6,20 +6,25 @@ import codes.ztereohype.mchue.devices.HueBridge;
 import codes.ztereohype.mchue.devices.HueLight;
 import codes.ztereohype.mchue.devices.interfaces.LightState;
 import codes.ztereohype.mchue.gui.screens.LightSelectionScreen;
-import codes.ztereohype.mchue.gui.widget.entries.LightEntry;
+import codes.ztereohype.mchue.util.DrawingUtil;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
+import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.locale.Language;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 
-public class LightSelectionList extends ObjectSelectionList<LightEntry> {
+public class LightSelectionList extends ObjectSelectionList<LightSelectionList.LightEntry> {
     private static final LightState BLINK_COLOUR = new LightState(32, 255, 128, true);
     private final LightSelectionScreen parent;
     private HueBridge selectedBridge;
@@ -365,5 +370,69 @@ public class LightSelectionList extends ObjectSelectionList<LightEntry> {
     @Override
     protected int getScrollbarPosition() {
         return this.width / 2 + 124 + x0;
+    }
+
+    public static class LightEntry extends Entry<LightEntry> {
+        private final Minecraft minecraft = Minecraft.getInstance();
+        private final @Getter HueLight light;
+        private final ResourceLocation LIGHT_ICON = new ResourceLocation(McHue.MOD_ID, "textures/gui/lightbulb_icon.png");
+        private final LightSelectionList parent;
+
+        public LightEntry(HueLight light, LightSelectionList parent) {
+            this.light = light;
+            this.parent = parent;
+        }
+
+        //todo
+        @Override
+        public Component getNarration() {
+            return null;
+        }
+
+        @Override
+        public void render(@NotNull PoseStack poseStack, int index, int y, int x, int rowWidth, int rowHeight, int mouseX, int mouseY, boolean isHovered, float delta) {
+            int ICON_SIZE = 32;
+
+            // icon
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+            RenderSystem.setShaderTexture(0, LIGHT_ICON);
+            RenderSystem.enableBlend();
+            blit(poseStack, x, y, 0.0F, 0.0F, ICON_SIZE, ICON_SIZE, ICON_SIZE, ICON_SIZE);
+            RenderSystem.disableBlend();
+
+            // title
+            Font font = this.minecraft.font;
+            Component name = Component.literal(light.getName());
+            this.minecraft.font.draw(poseStack, Language.getInstance()
+                                                        .getVisualOrder(name), x + 3 + ICON_SIZE, y + 1, 0xFFFFFF);
+
+            // label
+            if (parent.isSelectedItem(index)) {
+                String labelContent = "Selected";
+                int labelWidth = font.width(labelContent) + 6;
+                int labelX = x + 5 + ICON_SIZE + font.width(name);
+                int outlineColor = 0xff004499;
+                int backgroundColor = 0xff122f61;
+                int textColor = 0xffffff;
+
+                //outlines
+                fill(poseStack, labelX + 1, y - 1, labelX + labelWidth, y, outlineColor);
+                fill(poseStack, labelX, y, labelX + 1, y + Minecraft.getInstance().font.lineHeight, outlineColor);
+                fill(poseStack, labelX + 1, y + 1 + Minecraft.getInstance().font.lineHeight - 1, labelX + labelWidth, y + Minecraft.getInstance().font.lineHeight + 1, outlineColor);
+                fill(poseStack, labelX + labelWidth, y, labelX + labelWidth + 1, y + Minecraft.getInstance().font.lineHeight, outlineColor);
+                //background
+                fill(poseStack, labelX + 1, y, labelX + labelWidth, y + Minecraft.getInstance().font.lineHeight, backgroundColor);
+                //text
+                Minecraft.getInstance().font.draw(poseStack, labelContent, (labelX + 1 + (labelWidth - Minecraft.getInstance().font.width(labelContent)) / 2.0F), y + 1, textColor);
+            }
+
+            // description
+            String summary = "Index: " + light.getIndex() + System.lineSeparator() + "ID: " + light.getId();
+            DrawingUtil.drawStrings(this.minecraft, poseStack, summary, x + 7 + ICON_SIZE, y + minecraft.font.lineHeight + 2, rowWidth - 7, 2, 0x808080);
+        }
+
+        public boolean mouseClicked(double v, double v1, int i) {
+            return true;
+        }
     }
 }
